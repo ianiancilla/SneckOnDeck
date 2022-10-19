c7d9274af6d2ca5ee0fb05273977c4a6db339658
marzia_deck <marzia.faustinelli@gmail.com>
2022-10-19 22:56:00 +0200

    snaketail behaviour for movement, growing, and collisions

--- Assets/Scripts/Snake.cs
+++ Assets/Scripts/Snake.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using System;
+using System.Linq;
 
 using SnakeGame.GameLoop;
 using SnakeGame.GridSpace;
@@ -12,23 +13,29 @@ namespace SnakeGame.Snake
 {
     public class Snake : MonoBehaviour
     {
-        [Header("Snake")]
         [SerializeField] GameObject snakeTilePrefab;
-
-        [Space]
-        [Header("Gameplay")]
         [SerializeField] Direction startingDirection = Direction.North;
+        [SerializeField] int startingLength = 1;
 
         // variables
-        Direction currentDirection;
-        GameObject head;
-        float tickTimer;
+        Transform head;
+
+        Direction currentDirection;  //the direction the snake is actually going
+        Direction latestDirectionInput;  //mutliple inputs can be recorded between ticks
+       
+        int currentLength;
+        bool grewLengthThisTick = false;
+
+        List<Transform> snakeTileTransforms = new List<Transform>();
 
         void Awake()
         {
             currentDirection = startingDirection;
+            currentLength = startingLength;
 
-            head = Instantiate(snakeTilePrefab, Vector3.zero, Quaternion.identity);
+            // set head
+            head = Instantiate(snakeTilePrefab, Vector3.zero, Quaternion.identity).transform;
+            snakeTileTransforms.Add(head);
         }
 
         private void Start()
@@ -49,19 +56,19 @@ namespace SnakeGame.Snake
             {
                 case Vector2 v when v.Equals(Vector2.up) 
                 && currentDirection != Direction.South:
-                    currentDirection = Direction.North;
+                    latestDirectionInput = Direction.North;
                     break;
                 case Vector2 v when v.Equals(Vector2.down)
                 && currentDirection != Direction.North:
-                    currentDirection = Direction.South;
+                    latestDirectionInput = Direction.South;
                     break;
                 case Vector2 v when v.Equals(Vector2.left)
                 && currentDirection != Direction.East:
-                    currentDirection = Direction.West;
+                    latestDirectionInput = Direction.West;
                     break;
                 case Vector2 v when v.Equals(Vector2.right)
                 && currentDirection != Direction.West:
-                    currentDirection = Direction.East;
+                    latestDirectionInput = Direction.East;
                     break;
                 default:
                     break;
@@ -70,23 +77,69 @@ namespace SnakeGame.Snake
 
         private void GameManager_OnTick(object sender, EventArgs e)
         {
+            ApplyLatestInput();
             Move();
+        }
+
+        private void ApplyLatestInput()
+        {
+            currentDirection = latestDirectionInput;
         }
 
         private void Move()
         {
+            // find new head position
             GridPosition headPos = Grid.WorldPosToGridPos(head.transform.position);
             GridPosition newHeadPos = Grid.GetNeighbour(headPos, currentDirection);
 
-            if (Grid.IsValidGridPos(newHeadPos))
+            // if collided with screen border
+            if (!Grid.IsValidGridPos(newHeadPos))
             {
-                head.transform.position = Grid.GridPosToWorldPos(newHeadPos);
+                Die();
+                return;
             }
 
-            else
+            // add a tile on top of last one if one should be added
+            if (snakeTileTransforms.Count < currentLength)
             {
-                Debug.Log("SBAM");
+                Transform newTile = Instantiate(snakeTilePrefab, 
+                                                snakeTileTransforms.Last().position, 
+                                                Quaternion.identity).transform;
+
+                snakeTileTransforms.Add(newTile);
+                grewLengthThisTick = true;
             }
+
+            // update tail positions
+            int lastTileToUpdate = snakeTileTransforms.Count - 1;
+            if (grewLengthThisTick) // the last one does not move if just created
+            {
+                lastTileToUpdate --;
+                grewLengthThisTick = false;
+            }
+
+            // from the last one, each follows the position of the one previous, before that is updated
+            for (int i = lastTileToUpdate; i > 0; i--)
+            {
+                snakeTileTransforms[i].position = snakeTileTransforms[i-1].position;
+            }
+
+            // check for self-collision
+            foreach(Transform tile in snakeTileTransforms)
+            {
+                if (Grid.GridPosToWorldPos(newHeadPos) == tile.position)
+                {
+                    Die();
+                }
+            }
+
+            // update head position
+            head.position = Grid.GridPosToWorldPos(newHeadPos);
+        }
+
+        private static void Die()
+        {
+            Debug.Log("DEATH");
         }
     }
 
